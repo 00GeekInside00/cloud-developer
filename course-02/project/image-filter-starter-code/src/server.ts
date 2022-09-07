55237086794da08a42ad6bdeed1b563a1f84bdef
TCBG5222 <ahmed.helmi@orange.com>
2022-09-07 16:29:46 +0200

    feature: filtered image endpoint implemented

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,15 +9,13 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
+
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -29,18 +27,51 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
-  
+  // Implement /filterimage endpoint
+  // upload and process an image from url 
+  app.get("/filteredimage", async (req, res) => {
+
+    const { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send("Image url is required!");
+    }
+
+    const filtered_image_path = await filterImageFromURL(image_url)
+      .then((image_path) => {
+        res.status(200).sendFile(image_path, (err) => {
+          if (err) {
+            return res.status(500);
+          }
+          deleteLocalFiles([image_path])
+        });
+
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(404).send('Image not found!');
+      });
+
+  });
+
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
+    res.send("<marquee><h2>try GET <a href='/filteredimage?image_url='>/filteredimage?image_url={{}}</a></h2></marquee>")
+  });
+
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
